8dc602ecd06bb0eda4fd43829e543acb4f7e5bc5
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-01-17 15:53:17 +0100

    Toggle theme integration on auth modules

--- src/app/[locale]/(guest)/(routes)/auth/layout.tsx
+++ src/app/[locale]/(guest)/(routes)/auth/layout.tsx
@@ -1,11 +1,23 @@
+'use client'
+
+import { ToggleTheme } from "@/components/theme/Toggle"
+
 export default function Layout({ 
     children 
 } : { 
     children: React.ReactNode 
 }) {
     return (
-        <main className="h-screen w-screen flex flex-col items-center justify-center"> 
-            { children }
+        <main className="h-screen w-screen flex flex-col items-center">
+            {/* @ Toggle theme */}
+            <section className="fixed right-0 top-0 m-5">
+                <ToggleTheme />
+            </section>
+
+            {/* @ Content */}
+            <section className="flex-1 flex items-center">
+                { children }
+            </section>
         </main>
     )
 }
