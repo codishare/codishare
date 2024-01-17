4712e982419fe211c1946382a4dbe14dbf2e189d
Medinaa <themario92017@gmail.com>
2024-01-17 15:14:54 +0100

    Add navigation intl and resposive sign-up page

--- src/app/[locale]/(guest)/(routes)/auth/sign-up/page.tsx
+++ src/app/[locale]/(guest)/(routes)/auth/sign-up/page.tsx
@@ -1,9 +1,12 @@
 import { useTranslations } from "next-intl"
+import { ToogleTheme } from '@/components/theme/toogleTheme';
 
 export default function Page() {
     const t = useTranslations('Auth.SignUp')
 
-    return <div className="w-[400px]">
+    return <div className="w-full max-sm:px-10 sm:w-[450px]">
+        {/* <ToogleTheme /> */}
+
         <img 
             src="/assets/auth-avatar.gif"
             alt="Avatar"
@@ -11,18 +14,18 @@ export default function Page() {
             width={ 100 }
         />
 
-        <h1 className="mb-4 text-3xl mt-5 font-extrabold text-gray-900 dark:text-white md:text-5xl">
+        <h1 className="mt-5 font-extrabold text-gray-700 dark:text-white text-5xl text-balance">
             { t("header.white") } <span className="text-transparent bg-clip-text bg-gradient-to-r to-violet-400 from-indigo-500">{ t("header.colored") }</span>
         </h1>
 
-        <p className="text-lg font-normal text-gray-500 dark:text-gray-400">{ t("header.description") }</p>
+        <p className="mt-2 text-lg font-normal text-gray-500 dark:text-gray-400 text-balance">{ t("header.description") }</p>
 
         <section className="flex w-full gap-4 mt-5">
-            <button className="bg-indigo-500 text-sm flex-1 text-white rounded-full py-3 hover:bg-indigo-700 transition-all">
+            <button className="bg-sky-600 dark:bg-sky-600/40 dark:border-[1px] dark:border-sky-500 text-sm text-semibold flex-1 text-white rounded-full py-3 hover:bg-sky-500 hover:scale-95 transition">
                 Tengo cuenta
             </button>
 
-            <button className="border border-indigo-500 text-indigo-500 text-sm flex-1 rounded-full py-3 hover:scale-95 transition-all">
+            <button className="border border-[1px] border-inset border-sky-500 hover:bg-sky-500 dark:hover:bg-sky-600/40 text-sky-500 hover:text-white text-sm flex-1 rounded-full py-3 hover:scale-95 transition-all">
                 Comenzar
             </button>
         </section>
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,17 @@
 import { useTranslations } from 'next-intl';
+import {Link} from '../../navigation';
+import { ToogleTheme } from '@/components/theme/toogleTheme';
 
 export default function Index() {
     const t = useTranslations('Dashboard');
 
     return <div className='text-red-500'>
         { t('welcome') }, Developerrr
+        <br/>
+        <ToogleTheme />
+        <br/>
+        <Link href='/auth/sign-up'>
+            Sign Up
+        </Link>
     </div>
 }
