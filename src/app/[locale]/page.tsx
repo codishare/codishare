efe0a42b36d5c931d1360668e1237696b011b6b1
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-01-23 16:05:18 +0100

    Added unstable render on next-intl, prod build fix.

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,18 @@
 import { useTranslations } from 'next-intl';
 import {Link} from '../../navigation'; 
+import {unstable_setRequestLocale} from 'next-intl/server';
 
-export default function Index() {
+export default function Index({
+    params: {
+        locale
+    }
+} : {
+    params: {
+        locale: string;
+    };
+}) {
+    unstable_setRequestLocale(locale);
+    
     const t = useTranslations('Dashboard');
 
     return <div className='text-red-500'>  
