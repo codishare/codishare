73661be26762352a0094ccaa5e410ff125c23534
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-02-15 20:26:14 +0100

    Initial interface with preference information

---
+++ src/app/[locale]/(authorized)/(routes)/preferences/page.tsx
@@ -0,0 +1,26 @@
+import Header from "@/components/layouts/preferences/header"; 
+import { ChevronLeftOutlined, SettingsAccessibilityOutlined } from "@mui/icons-material";
+
+export default function Page() {
+    return <div className="flex flex-col gap-5 mt-5 items-center self-center w-2/3">
+        {/* @ Actions */}
+        <section className="flex items-center gap-5 justify-between w-full">
+            <button className="p-2 flex items-center gap-3 rounded-lg bg-white border transition-all">
+                <ChevronLeftOutlined />
+
+                Go back
+            </button>
+
+            <button className="p-2 flex items-center gap-3 rounded-lg border bg-white transition-all">
+                <SettingsAccessibilityOutlined />
+
+                Start editing
+            </button> 
+        </section>
+
+        {/* @ Profile information */}
+        <Header />
+
+        {/* @ User devices */}
+    </div>
+}
---
+++ src/components/layouts/preferences/header.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { Session } from "@/_types"
+import { useSession } from "@/lib/hooks/useSession"
+
+export default function Header() {
+    const {
+        session
+    }: {
+        session: Session | false;
+    } = useSession();
+
+    if(!session) return null;
+
+    return <section className="w-full gap-7 flex items-center bg-white rounded border p-5">
+        <img 
+            className="h-12 w-12 object-cover rounded-full shadow-lg transition-all"
+            src="https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg"
+        />
+
+        <article className="flex flex-col">
+            <h1 className="font-bold">
+                {session.name}
+            </h1>
+
+            <p className="text-gray-500 text-xs">
+                @{ session.alias }
+            </p>
+        </article>
+    </section>
+}
