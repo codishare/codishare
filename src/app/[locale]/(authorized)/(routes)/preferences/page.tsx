3c3069a933f3b2b60e8246d4eca46cec00c56e49
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-02-20 16:20:24 +0100

    Changed edit mode to view profile

--- src/app/[locale]/(authorized)/(routes)/preferences/page.tsx
+++ src/app/[locale]/(authorized)/(routes)/preferences/page.tsx
@@ -1,21 +1,25 @@
 import Content from "@/components/layouts/preferences/content";
 import Header from "@/components/layouts/preferences/header"; 
-import { ChevronLeftOutlined, SettingsAccessibilityOutlined } from "@mui/icons-material";
+import { Link } from "@/navigation";
+import { ChevronLeftOutlined, RocketLaunchOutlined } from "@mui/icons-material";
 
 export default function Page() {
     return <div className="flex flex-col gap-5 mt-5 items-center self-center w-full px-5 lg:px-0 lg:w-2/3">
         {/* @ Actions */}
         <section className="flex items-center gap-5 justify-between w-full">
-            <button className="p-2 flex items-center gap-3 rounded-lg bg-white border transition-all">
+            <Link 
+                href="/"
+                className="p-2 flex items-center gap-3 rounded-lg bg-white border transition-all"
+            >
                 <ChevronLeftOutlined />
 
                 Go back
-            </button>
+            </Link>
 
             <button className="p-2 flex items-center gap-3 rounded-lg border bg-white transition-all">
-                <SettingsAccessibilityOutlined />
+                <RocketLaunchOutlined />
 
-                Start editing
+                View profile
             </button> 
         </section>
 
