249bb692a6a5efd98f95ee582e001a37c9bb4902
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-02-20 15:28:06 +0100

    Link to preferences

--- src/components/layouts/settings/profile.tsx
+++ src/components/layouts/settings/profile.tsx
@@ -1,11 +1,13 @@
 import { AdminPanelSettingsOutlined } from "@mui/icons-material";
+import { Link } from "@/navigation";
 
 export default function Profile() {
-    return <li
+    return <Link
+        href="/preferences"
         className="flex gap-3 items-center cursor-pointer bg-zinc-100 dark:bg-zinc-800 text-sm p-2.5 hover:bg-indigo-500 hover:text-white transition rounded-lg"
     >
         <AdminPanelSettingsOutlined />
 
         Profile
-    </li>
+    </Link>
 }
