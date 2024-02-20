55c954b130bc8fb1e026f18c1fb5e8868b5f1aa8
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-02-20 23:10:31 +0100

    Translated aside settings language module

--- src/components/layouts/settings/language.tsx
+++ src/components/layouts/settings/language.tsx
@@ -2,20 +2,23 @@
 
 import { ExpandMoreOutlined } from "@mui/icons-material";
 import { useRouter, usePathname } from '@/navigation'; 
+import { useTranslations } from "next-intl";
 
 export default function Language() {
     const router = useRouter();
     const pathname = usePathname();  
 
+    const t = useTranslations('Components.UI.aside_settings')
+
     const switchLang = (lang: string) => router.push(pathname, { locale: lang });
 
     return <li className="flex flex-col gap-3 group transition-all">
         <section
-            className="flex gap-3 items-center cursor-pointer bg-zinc-100 dark:bg-zinc-800 text-sm p-2.5 hover:bg-indigo-500 hover:text-white transition rounded-lg"
+            className="flex gap-3 items-center cursor-pointer bg-zinc-100 dark:bg-zinc-800 text-sm p-2.5 hover:bg-indigo-500 hover:text-white transition rounded-lg dark:hover:bg-indigo-500 dark:hover:text-white"
         >
             <ExpandMoreOutlined />
 
-            Español
+            { t('language') }
         </section>
 
         <section className="flex-col gap-3 hidden group-hover:flex transition-all">
