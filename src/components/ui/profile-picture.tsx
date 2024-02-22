3bd5feca26793d52402e5e51b948c0297d80bde6
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-02-22 22:52:12 +0100

    Profile picture component

---
+++ src/components/ui/profile-picture.tsx
@@ -0,0 +1,23 @@
+import { cn } from "@/utils/cn";
+import Image from "next/image";
+
+export default function ProfilePicture({
+    icon,
+    height = 48,
+    width = 48,
+    className
+}: {
+    icon: string;
+    height?: number;
+    width?: number;
+    className?: string;
+}) {
+    return <Image
+        src={ icon.replace(/\\/g, '/') }
+        alt=""
+        width={ width }
+        height={ height }
+        onError={(e) => e.target.classList.add('hidden')}
+        className={ cn('object-cover rounded-full shadow-lg', className) }
+    />
+}
