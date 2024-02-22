c08cf8209be683e3d5b1af3131adf857ecd49b06
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-02-22 23:05:58 +0100

    Undo notifications from content

--- src/components/layouts/preferences/content.tsx
+++ src/components/layouts/preferences/content.tsx
@@ -23,9 +23,6 @@ export default function Content() {
         {
             label: t('devices'), 
             element: <Devices />
-        },
-        {
-            label: t('notifications')
         }
     ]
 
