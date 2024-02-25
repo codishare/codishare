80235d882099e7fb368a5abb64ab49505b853f45
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-02-25 18:47:50 +0100

    Register validations on controller

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,49 +1,13 @@
-import { NextResponse } from "next/server";
-import type { SignUp } from "@/types/auth/_types";
-import { validate } from "@/lib/services/shared/layouts/auth/sign-up";
-import { countUsers, isEmailInUse } from "@/lib/services/api/auth/user";
-import prisma from "@/lib/prisma";
-import { encrypt } from "@/lib/encryption";
-import { Seniority, Stack } from "@prisma/client";
+import { NextResponse } from "next/server"; 
+import register from "@/lib/services/api/auth/register";
 
 export async function POST(
     req: Request
 ) {
     try {
-        const data = await req.json() as SignUp; 
+        const response = await register(req)
 
-        const isValid = validate(data);
-
-        if(isValid !== true) return NextResponse.json({
-            message: isValid
-        }, {
-            status: 400
-        })
-
-        const {
-            name, 
-            email, 
-            password, 
-            stack, 
-            role
-        } = data; 
-
-        if(await isEmailInUse(email)) return NextResponse.json({
-            message: 'email_in_use'
-        }, {
-            status: 400
-        })
-
-        await prisma.user.create({
-            data: {
-                name, 
-                email, 
-                password: encrypt(password),
-                role: await countUsers() === 0 ? 'ADMIN' : 'USER', 
-                stack: stack as Stack | null | undefined,
-                seniority: role as Seniority | null | undefined
-            }
-        })
+        if(response instanceof NextResponse) return response
 
         return NextResponse.json({
             message: 'account_created'
