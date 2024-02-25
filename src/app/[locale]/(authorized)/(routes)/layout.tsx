b3805b59f0833ab91151c0f95a1a71d132dac3dc
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-02-25 17:54:13 +0100

    Removed session validator on authorized layout

--- src/app/[locale]/(authorized)/(routes)/layout.tsx
+++ src/app/[locale]/(authorized)/(routes)/layout.tsx
@@ -1,24 +1,8 @@
-"use client";
+'use server'
 
-import { Session } from "@/types/auth/_types";
-import Navigator from "@/components/layouts/navigator"; 
-import { useSession } from "@/lib/hooks/useSession";
-import { useRouter } from "@/navigation";
+import Navigator from "@/components/layouts/navigator";  
 
-export default function Layout({ children }: { children: React.ReactNode }) {
-    const {
-        session,
-        loading,
-    }: {
-        session: Session | false;
-        loading: boolean;
-    } = useSession();
-    const router = useRouter();
-
-    if (!session && !loading) {
-        return router.push("/auth/login");
-    }
-
+export default async function Layout({ children }: { children: React.ReactNode }) {
     return (
         <main className="w-screen min-h-screen bg-gray-100 dark:bg-zinc-950 flex flex-col gap-3">
             <Navigator />
