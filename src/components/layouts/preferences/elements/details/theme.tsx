8b5ed6271b70c85d613166a09d8ca547dbfb293d
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-02-25 17:18:06 +0100

    Green color on selected theme

--- src/components/layouts/preferences/elements/details/theme.tsx
+++ src/components/layouts/preferences/elements/details/theme.tsx
@@ -41,42 +41,42 @@ export default function Theme() {
         {/* @ Themes */}
         <section className="flex items-center gap-5 mt-5 flex-col md:flex-row">
             {/* @ Light */}
-            <article onClick={() => setTheme('light')} className="border p-5 bg-white cursor-pointer group hover:border-indigo-500 transition-all rounded-md w-full md:w-auto md:flex-1">
+            <article onClick={() => setTheme('light')} className={ `border p-5 bg-white cursor-pointer group hover:border-indigo-500 transition-all rounded-md w-full md:w-auto md:flex-1 ${ theme == 'light' ? 'border-green-500' : '' }` }>
                 <h3 className="text-lg font-bold">
-                    <SkeletonUI className="w-20 h-5 group-hover:bg-indigo-500 transition-all" />
+                    <SkeletonUI className={ `w-20 h-5 group-hover:bg-indigo-500 transition-all ${ theme == 'light' ? 'bg-green-500' : '' }` } />
                 </h3>
 
                 <small className="text-gray-400">
-                    <SkeletonUI className="w-40 h-3 group-hover:bg-indigo-500 transition-all" />
+                    <SkeletonUI className={ `w-40 h-3 group-hover:bg-indigo-500 transition-all ${ theme == 'light' ? 'bg-green-500' : '' }` } />
                 </small>
 
-                <SkeletonUI className="w-40 h-10 rounded-md group-hover:bg-indigo-500 transition-all" />
+                <SkeletonUI className={ `w-40 h-10 rounded-md group-hover:bg-indigo-500 transition-all ${ theme == 'light' ? 'bg-green-500' : '' }` } />
 
                 <div className="flex gap-3">
-                    <SkeletonUI className="w-10 h-10 rounded-md group-hover:bg-indigo-500 transition-all" />
-                    <SkeletonUI className="w-10 h-10 rounded-md group-hover:bg-indigo-500 transition-all" />
-                    <SkeletonUI className="w-10 h-10 rounded-md group-hover:bg-indigo-500 transition-all" />
-                    <SkeletonUI className="w-10 h-10 rounded-md group-hover:bg-indigo-500 transition-all" />
+                    <SkeletonUI className={ `w-10 h-10 rounded-md group-hover:bg-indigo-500 transition-all ${ theme == 'light' ? 'bg-green-500' : '' }` } />
+                    <SkeletonUI className={ `w-10 h-10 rounded-md group-hover:bg-indigo-500 transition-all ${ theme == 'light' ? 'bg-green-500' : '' }` } />
+                    <SkeletonUI className={ `w-10 h-10 rounded-md group-hover:bg-indigo-500 transition-all ${ theme == 'light' ? 'bg-green-500' : '' }` } />
+                    <SkeletonUI className={ `w-10 h-10 rounded-md group-hover:bg-indigo-500 transition-all ${ theme == 'light' ? 'bg-green-500' : '' }` } />
                 </div>
             </article>
 
             {/* @ Dark */}
-            <article onClick={() => setTheme('dark')} className="border cursor-pointer group hover:border-indigo-500 transition-all border-zinc-600 p-5 rounded-md bg-zinc-700 w-full md:w-auto md:flex-1">
+            <article onClick={() => setTheme('dark')} className={ `border cursor-pointer group hover:border-indigo-500 transition-all border-zinc-600 p-5 rounded-md bg-zinc-700 w-full md:w-auto md:flex-1 ${ theme == 'dark' ? 'border-green-500' : '' }` }>
                 <h3 className="text-lg font-bold">
-                    <SkeletonUI className="w-20 bg-gray-500 h-5 group-hover:bg-indigo-500 transition-all" />
+                    <SkeletonUI className={ `w-20 bg-gray-500 h-5 group-hover:bg-indigo-500 transition-all ${ theme == 'dark' ? 'bg-green-500' : '' }` } />
                 </h3>
 
                 <small className="text-gray-400">
-                    <SkeletonUI className="w-40 bg-gray-500 h-3 group-hover:bg-indigo-500 transition-all" />
+                    <SkeletonUI className={ `w-40 bg-gray-500 h-3 group-hover:bg-indigo-500 transition-all ${ theme == 'dark' ? 'bg-green-500' : '' }` } />
                 </small>
 
-                <SkeletonUI className="w-40 h-10 bg-gray-500 rounded-md group-hover:bg-indigo-500 transition-all" />
+                <SkeletonUI className={ `w-40 h-10 bg-gray-500 rounded-md group-hover:bg-indigo-500 transition-all ${ theme == 'dark' ? 'bg-green-500' : '' }` } />
 
                 <div className="flex gap-3">
-                    <SkeletonUI className="w-10 bg-gray-500 h-10 rounded-md group-hover:bg-indigo-500 transition-all" />
-                    <SkeletonUI className="w-10 bg-gray-500 h-10 rounded-md group-hover:bg-indigo-500 transition-all" />
-                    <SkeletonUI className="w-10 bg-gray-500 h-10 rounded-md group-hover:bg-indigo-500 transition-all" />
-                    <SkeletonUI className="w-10 bg-gray-500 h-10 rounded-md group-hover:bg-indigo-500 transition-all" />
+                    <SkeletonUI className={ `w-10 bg-gray-500 h-10 rounded-md group-hover:bg-indigo-500 transition-all ${ theme == 'dark' ? 'bg-green-500' : '' }` } />
+                    <SkeletonUI className={ `w-10 bg-gray-500 h-10 rounded-md group-hover:bg-indigo-500 transition-all ${ theme == 'dark' ? 'bg-green-500' : '' }` } />
+                    <SkeletonUI className={ `w-10 bg-gray-500 h-10 rounded-md group-hover:bg-indigo-500 transition-all ${ theme == 'dark' ? 'bg-green-500' : '' }` } />
+                    <SkeletonUI className={ `w-10 bg-gray-500 h-10 rounded-md group-hover:bg-indigo-500 transition-all ${ theme == 'dark' ? 'bg-green-500' : '' }` } />
                 </div>
             </article>
         </section>
