37c49a2bd4c67f71924a0a72a5b52efcc576ed94
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-02-26 23:35:34 +0100

    Options handler and credentials provider

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,9 @@
 import { NextAuthOptions } from 'next-auth';
 import NextAuth from 'next-auth/next';
 import GithubProvider from 'next-auth/providers/github';
+import CredentialsProvider from "next-auth/providers/credentials";
 
-export const options: NextAuthOptions = {
+export const authOptions: NextAuthOptions = {
     secret: process.env.NEXTAUTH_SECRET as string,
 
     session: {
@@ -10,14 +11,28 @@ export const options: NextAuthOptions = {
     },
     
     providers: [
+        CredentialsProvider({
+            type: "credentials",
+            
+            credentials: {}, 
+
+            authorize: async () => {
+                return null
+            }
+        }),
+
         GithubProvider({
             clientId: process.env.GITHUB_CLIENT_ID as string,
             clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
         }),
     ],
+
+    pages: {
+        signIn: '/auth/signin'
+    }
 };
 
-export const handler = NextAuth(options);
+export const handler = NextAuth(authOptions);
 
 export {
     handler as GET, 
