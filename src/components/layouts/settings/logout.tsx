8e33c78544ef9a32315c0652d65c307b52b8b5bb
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-02-26 18:59:30 +0100

    Removed unworking session close unload

--- src/components/layouts/settings/logout.tsx
+++ src/components/layouts/settings/logout.tsx
@@ -3,22 +3,20 @@
 import { useNotifications } from "@/lib/hooks/useNotifications"; 
 import PersonOutlineIcon from '@mui/icons-material/PersonOutline'; 
 import { DnsOutlined } from "@mui/icons-material";
-import { useTranslations } from "next-intl";
-import { useRouter } from "@/navigation";
+import { useTranslations } from "next-intl"; 
+import { useState } from "react"; 
 
-export default function Logout({
-    handleLoad
-} : {
-    handleLoad: (loading: boolean) => void;
-}) {
-    const addNotification = useNotifications(); 
-    const router = useRouter(); 
+export default function Logout() {
+    const [loading, handleLoad] = useState(false);
+    const addNotification = useNotifications();  
 
     const t = useTranslations(); 
 
     return <li
         className="flex gap-3 items-center cursor-pointer bg-zinc-100 dark:bg-zinc-800 text-sm p-2.5 hover:bg-indigo-500 hover:text-white dark:hover:bg-indigo-500 dark:hover:text-white transition rounded-lg"
         onClick={() => {
+            if(loading) return;
+
             try {
                 handleLoad(true);
 
@@ -32,8 +30,7 @@ export default function Logout({
                     const data = await res.json();
 
                     if (res.status == 200) {
-                        localStorage.removeItem('access_token');
-                        router.push('/auth/login'); 
+                        localStorage.removeItem('access_token'); 
                     } else {
                         addNotification({
                             type: "ERROR",
