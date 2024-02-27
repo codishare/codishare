887aa13634bb5c275fdaca5403653f52c135f19b
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-02-27 16:32:37 +0100

    Removed unused import server session

--- src/lib/providers/index.tsx
+++ src/lib/providers/index.tsx
@@ -1,12 +1,9 @@
 "use server";
 
-import TrpcProvider from "@/app/_trpc/TrpcProvider"; 
-import { getServerSession } from "next-auth";
+import TrpcProvider from "@/app/_trpc/TrpcProvider";  
 import AuthProvider from "./auth";
 
-export const Providers = async ({ children }: { children: React.ReactNode }) => {
-    const session = await getServerSession()
-
+export const Providers = async ({ children }: { children: React.ReactNode }) => { 
     return (
         <TrpcProvider>
             {/* <ThemeProvider attribute="class"> */}
