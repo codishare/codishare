dc07d2b7a21fd0234bcb13542a78893ee28f7123
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-02-28 18:47:17 +0100

    Prevent invalid session access on authorized module

--- src/app/[locale]/(authorized)/(routes)/layout.tsx
+++ src/app/[locale]/(authorized)/(routes)/layout.tsx
@@ -1,8 +1,14 @@
 'use server'
 
+import { auth } from "@/auth";
 import Navigator from "@/components/layouts/navigator";
+import { redirect } from "@/navigation";
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
+    const session = await auth();
+
+    if(!session) return redirect('/auth/signin');
+
     return (
         <main className="w-screen min-h-screen bg-gray-100 dark:bg-zinc-950 flex flex-col gap-3">
             <Navigator />
